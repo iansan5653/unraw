5cda9c1a58a9e1c740bc941f19ddde64b3dd3a42
Ian Sanders <iansan5653@gmail.com>
2019-08-02 19:07:20 -0400

    Add tests for hexadecimal sequences

--- src/index.test.ts
+++ src/index.test.ts
@@ -194,5 +194,179 @@ context("unraw", function(): void {
         assert.strictEqual(unraw(raw`test\$test`), `test\$test`);
       });
     });
+
+    context("\\A", function(): void {
+      it("should parse alone", function(): void {
+        assert.strictEqual(unraw(raw`\A`), `\A`);
+      });
+      it("should parse with text after", function(): void {
+        assert.strictEqual(unraw(raw`\Atest`), `\Atest`);
+      });
+      it("should parse with text before", function(): void {
+        assert.strictEqual(unraw(raw`test\A`), `test\A`);
+      });
+      it("should parse with text around", function(): void {
+        assert.strictEqual(unraw(raw`test\Atest`), `test\Atest`);
+      });
+    });
+
+    describe("handles deeper escape levels", function(): void {
+      context("\\\\t (even number of escapes)", function(): void {
+        it("should parse alone", function(): void {
+          assert.strictEqual(unraw(raw`\\t`), `\\t`);
+        });
+        it("should parse with text after", function(): void {
+          assert.strictEqual(unraw(raw`\\ttest`), `\\ttest`);
+        });
+        it("should parse with text before", function(): void {
+          assert.strictEqual(unraw(raw`test\\t`), `test\\t`);
+        });
+        it("should parse with text around", function(): void {
+          assert.strictEqual(unraw(raw`test\\ttest`), `test\\ttest`);
+        });
+      });
+
+      context("\\\\\\t (odd number of escapes)", function(): void {
+        it("should parse alone", function(): void {
+          assert.strictEqual(unraw(raw`\\\t`), `\\\t`);
+        });
+        it("should parse with text after", function(): void {
+          assert.strictEqual(unraw(raw`\\\ttest`), `\\\ttest`);
+        });
+        it("should parse with text before", function(): void {
+          assert.strictEqual(unraw(raw`test\\\t`), `test\\\t`);
+        });
+        it("should parse with text around", function(): void {
+          assert.strictEqual(unraw(raw`test\\\ttest`), `test\\\ttest`);
+        });
+      });
+    });
+  });
+
+  describe("handles hexadecimal escape sequences", function(): void {
+    context("\\x00 (minimum possible value)", function(): void {
+      it("should parse alone", function(): void {
+        assert.strictEqual(unraw(raw`\x00`), `\x00`);
+      });
+      it("should parse with text after", function(): void {
+        assert.strictEqual(unraw(raw`\x00test`), `\x00test`);
+      });
+      it("should parse with text before", function(): void {
+        assert.strictEqual(unraw(raw`test\x00`), `test\x00`);
+      });
+      it("should parse with text around", function(): void {
+        assert.strictEqual(unraw(raw`test\x00test`), `test\x00test`);
+      });
+    });
+
+    context("\\x5A (typical value)", function(): void {
+      it("should parse alone", function(): void {
+        assert.strictEqual(unraw(raw`\x5A`), `\x5A`);
+      });
+      it("should parse with text after", function(): void {
+        assert.strictEqual(unraw(raw`\x5Atest`), `\x5Atest`);
+      });
+      it("should parse with text before", function(): void {
+        assert.strictEqual(unraw(raw`test\x5A`), `test\x5A`);
+      });
+      it("should parse with text around", function(): void {
+        assert.strictEqual(unraw(raw`test\x5Atest`), `test\x5Atest`);
+      });
+    });
+
+    context("\\xFF (minimum possible value)", function(): void {
+      it("should parse alone", function(): void {
+        assert.strictEqual(unraw(raw`\xFF`), `\xFF`);
+      });
+      it("should parse with text after", function(): void {
+        assert.strictEqual(unraw(raw`\xFFtest`), `\xFFtest`);
+      });
+      it("should parse with text before", function(): void {
+        assert.strictEqual(unraw(raw`test\xFF`), `test\xFF`);
+      });
+      it("should parse with text around", function(): void {
+        assert.strictEqual(unraw(raw`test\xFFtest`), `test\xFFtest`);
+      });
+    });
+
+    describe("handles deeper escape levels", function(): void {
+      context("\\\\x5A (even number of escapes)", function(): void {
+        it("should parse alone", function(): void {
+          assert.strictEqual(unraw(raw`\\x5A`), `\\x5A`);
+        });
+        it("should parse with text after", function(): void {
+          assert.strictEqual(unraw(raw`\\x5Atest`), `\\x5Atest`);
+        });
+        it("should parse with text before", function(): void {
+          assert.strictEqual(unraw(raw`test\\x5A`), `test\\x5A`);
+        });
+        it("should parse with text around", function(): void {
+          assert.strictEqual(unraw(raw`test\\x5Atest`), `test\\x5Atest`);
+        });
+      });
+
+      context("\\\\\\x5A (odd number of escapes)", function(): void {
+        it("should parse alone", function(): void {
+          assert.strictEqual(unraw(raw`\\\x5A`), `\\\x5A`);
+        });
+        it("should parse with text after", function(): void {
+          assert.strictEqual(unraw(raw`\\\x5Atest`), `\\\x5Atest`);
+        });
+        it("should parse with text before", function(): void {
+          assert.strictEqual(unraw(raw`test\\\x5A`), `test\\\x5A`);
+        });
+        it("should parse with text around", function(): void {
+          assert.strictEqual(unraw(raw`test\\\x5Atest`), `test\\\x5Atest`);
+        });
+      });
+    });
+
+    describe("errors on invalid sequences", function(): void {
+      context("\\x5 (wrong number of digits)", function(): void {
+        it("should error alone", function(): void {
+          assert.throws(function(): void {
+            unraw(raw`\\x5`);
+          }, new TypeError("malformed hexadecimal character escape sequence"));
+        });
+        it("should error with text after", function(): void {
+          assert.throws(function(): void {
+            unraw(raw`\\x5test`);
+          }, new TypeError("malformed hexadecimal character escape sequence"));
+        });
+        it("should error with text before", function(): void {
+          assert.throws(function(): void {
+            unraw(raw`test\\x5`);
+          }, new TypeError("malformed hexadecimal character escape sequence"));
+        });
+        it("should error with text around", function(): void {
+          assert.throws(function(): void {
+            unraw(raw`test\\x5test`);
+          }, new TypeError("malformed hexadecimal character escape sequence"));
+        });
+      });
+
+      context("\\x$$ (non-hex characters)", function(): void {
+        it("should error alone", function(): void {
+          assert.throws(function(): void {
+            unraw(raw`\\x$$`);
+          }, new TypeError("malformed hexadecimal character escape sequence"));
+        });
+        it("should error with text after", function(): void {
+          assert.throws(function(): void {
+            unraw(raw`\\x$$test`);
+          }, new TypeError("malformed hexadecimal character escape sequence"));
+        });
+        it("should error with text before", function(): void {
+          assert.throws(function(): void {
+            unraw(raw`test\\x$$`);
+          }, new TypeError("malformed hexadecimal character escape sequence"));
+        });
+        it("should error with text around", function(): void {
+          assert.throws(function(): void {
+            unraw(raw`test\\x$$test`);
+          }, new TypeError("malformed hexadecimal character escape sequence"));
+        });
+      });
+    });
   });
 });
